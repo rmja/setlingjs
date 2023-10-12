05a16255c0c627dbed7154b9c76d1eb0b6ed78ff
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-10-12 14:43:36 +0200

    Allow part to be created with "+" and "-"

--- src/parts/offset-part.ts
+++ src/parts/offset-part.ts
@@ -3,22 +3,31 @@ import { DateTime, Duration } from "luxon";
 import { IPart } from "./part";
 
 export class OffsetPart implements IPart {
-    constructor(public sign: -1 | 1, public duration: Duration) {
-    }
+  sign: -1 | 1;
+  
+  constructor(
+    sign: -1 | 1 | "+" | "-",
+    public duration: Duration
+  ) {
+    this.sign = sign === -1 || sign == "-" ? -1 : 1;
+  }
 
-    apply(origin: DateTime): DateTime {
-        return this.sign > 0
-            ? origin.plus(this.duration)
-            : this.sign < 0
-                ? origin.minus(this.duration)
-                : origin;
-    }
+  apply(origin: DateTime): DateTime {
+    return this.sign > 0
+      ? origin.plus(this.duration)
+      : this.sign < 0
+      ? origin.minus(this.duration)
+      : origin;
+  }
 
-    toRuleString(forcePrefixWithSeparator: boolean) {
-        return (this.sign === -1 ? "-" : forcePrefixWithSeparator ? "+" : "") + this.duration.toISO();
-    }
+  toRuleString(forcePrefixWithSeparator: boolean) {
+    return (
+      (this.sign === -1 ? "-" : forcePrefixWithSeparator ? "+" : "") +
+      this.duration.toISO()
+    );
+  }
 
-    toHuman(): string {
-        return (this.sign === -1 ? "- " : "+ ") + this.duration.toHuman();
-    }
+  toHuman(): string {
+    return (this.sign === -1 ? "- " : "+ ") + this.duration.toHuman();
+  }
 }
