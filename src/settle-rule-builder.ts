d23ead4e7852c9ddfae46d4952b28e5f8a4811fc
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-10-12 14:53:05 +0200

    Add SettleRuleBuilder.add

--- src/settle-rule-builder.ts
+++ src/settle-rule-builder.ts
@@ -1,11 +1,11 @@
-import { SettleRule } from "./settle-rule";
-
 import { Duration } from "luxon";
-import { NearestPart } from "./parts/nearest-part";
-import { StartOfPart } from "./parts/start-of-part";
-import { OffsetPart } from "./parts/offset-part";
-import { SettleUnit } from "./settle-unit";
 import { EndOfPart } from "./parts/end-of-part";
+import { NearestPart } from "./parts/nearest-part";
+import { OffsetPart } from "./parts/offset-part";
+import { Part } from "./parts/part";
+import { SettleRule } from "./settle-rule";
+import { SettleUnit } from "./settle-unit";
+import { StartOfPart } from "./parts/start-of-part";
 
 export class SettleRuleBuilder {
   private _rule = new SettleRule();
@@ -14,32 +14,37 @@ export class SettleRuleBuilder {
     return this._rule;
   }
 
+  add(part: Part) {
+    this._rule.parts.push(part);
+    return this;
+  }
+
   startOf(unit: SettleUnit) {
-    this.rule.parts.push(new StartOfPart(unit));
+    this.add(new StartOfPart(unit));
     return this;
   }
 
   nearest(unit: SettleUnit) {
-    this.rule.parts.push(new NearestPart(unit));
+    this.add(new NearestPart(unit));
     return this;
   }
 
   endOf(unit: SettleUnit) {
-    this.rule.parts.push(new EndOfPart(unit));
+    this.add(new EndOfPart(unit));
     return this;
   }
 
   plus(duration: Duration | string) {
     const value =
       typeof duration === "string" ? Duration.fromISO(duration) : duration;
-    this.rule.parts.push(new OffsetPart(1, value));
+    this.add(new OffsetPart(1, value));
     return this;
   }
 
   minus(duration: Duration | string) {
     const value =
       typeof duration === "string" ? Duration.fromISO(duration) : duration;
-    this.rule.parts.push(new OffsetPart(-1, value));
+    this.add(new OffsetPart(-1, value));
     return this;
   }
 }
